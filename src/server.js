3e910d492cda903642b59a7b2c79d0287c1129ed
Roberto Payan <kirindu3@gmail.com>
2021-10-31 05:35:22 -0600

    update for heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('/dist/userSkills/'));
+app.use(express.static(__dirname+'/dist/userSkills/'));
 app.get('/',function(req,res){
-    res.sendFile(path.join('/dist/userSkills//index.html'));
+    res.sendFile(path.join(__dirname+'/dist/userSkills//index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
