45fa94311f44bb6f599566dce27ad2852f4bcd9b
Roberto Payan <kirindu3@gmail.com>
2021-10-31 06:01:59 -0600

    update for heroku

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const ngApp = express();
 
-app.use(express.static(__dirname+'/dist/userSkills/'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/userSkills/index.html'));
+ngApp.use(express.static('./dist/userSkills'));
+
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/userSkills/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+ngApp.listen(process.env.PORT || 8080);
